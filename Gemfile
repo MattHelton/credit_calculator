27042049d7703d0842f0fd00173491e5977ec2c6
Matthew Helton <mjoelhelton@me.com>
2018-03-01 00:12:50 -0500

    Adds user model and seed data

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'omniauth-quickbooks'
 gem 'rest-client'
 gem 'shog'
 
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!({email: "address@email.com", password: "password", encrypted_password: "password"})
+User.create!({email: "emailaddress@email.com", password:"password", encrypted_password: "password"})
